a9198f9ba95378ab5b510488cc5393b8140fd1d2
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-03-10 17:45:03 +0000

    add  Doctors

---
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -0,0 +1,22 @@
+// // import React from "react";
+// //import { doctors } from "./../../assets/data/doctors";
+// const DoctorCard = ({ doctors }) => {
+//   const {
+//     name,
+//     avgRating,
+//     totalRating,
+//     photo,
+//     specialization,
+//     totalPatient,
+//     hospital,
+//   } = doctor;
+//   return (
+//     <div className="p-3 lg:p-5">
+//       <div>
+//         <img src="" alt="" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DoctorCard;
