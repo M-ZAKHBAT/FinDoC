75597082eedd6facb85945a5a4395ba9d671e7cf
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-03-10 15:50:32 +0000

    add Services

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,9 +5,12 @@ import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
+import featureImg from "../assets/images/feature-img.png";
+import videoIcon from "../assets/images/video-icon.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
+import ServiceList from "../components/Services/ServiceList";
 const Home = () => {
   return (
     <>
@@ -76,7 +79,7 @@ const Home = () => {
                 Providing the best medical services
               </h2>
 
-              <p className="text_para text-center">
+              <p className="text__para text-center">
                 World-class care for everyone. Our health System offers
                 unmatched, expert health care.
               </p>
@@ -163,9 +166,63 @@ const Home = () => {
                 unmatched, expert health care.
               </p>
             </div>
+            <ServiceList />
           </div>
         </section>
         {/* services section end */}
+        {/* feature section*/}
+        <section>
+          <div className="container">
+            <div className="flex items-center justify-between flex-col lg:flex-row">
+              {/* feature content*/}
+              <div className="xl:w-[670px]">
+                <h2 className="heading">
+                  Get Virtual Treatment <br /> anytime
+                </h2>
+                <ul className="pl-4">
+                  <li className="text__para">
+                    1. Schedule the appointment directly.
+                  </li>
+                  <li className="text__para">
+                    2. Search for your physician here, and contact their office.
+                  </li>
+                  <li className="text__para">
+                    3. view our physicians who are accepting new patients, use
+                    the online scheduling tool to select an appointment time.
+                  </li>
+                </ul>
+                <Link to="/">
+                  <button className="btn">Learn More</button>
+                </Link>
+              </div>
+              {/* feature IMG*/}
+              <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+                <img src={featureImg} className="w-3/4" alt="" />
+                <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:pb-[26px] rounded-[10px]">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-[6px] lg:gap-3">
+                      <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                        Tue,24
+                      </p>
+                      <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[400]">
+                        10:00AM
+                      </p>
+                    </div>
+                    <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                      <img src={videoIcon} alt="" />
+                    </span>
+                  </div>
+
+                  <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 ☐ text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                    {" "}
+                    Consultation
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* feature section end*/}
       </>
     </>
   );
