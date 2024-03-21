a0eaa49a9ac9d2063a5138151a81bf44c5a64287
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-03-21 22:51:47 +0000

    add authController and getJwt

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -3,17 +3,27 @@ import Doctor from "../models/DoctorSchema.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "15d",
+    }
+  );
+};
+
 export const register = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body;
-
+  // console.log(req.body);
   try {
     let user = null;
     if (role === "patient") {
-      user = User.findOne({ email });
+      user = await User.findOne({ email });
     } else if (role === "doctor") {
-      user = Doctor.findOne({ email });
+      user = await Doctor.findOne({ email });
     }
-
+    console.log(user);
     // tester si user Existe
     if (user) {
       return res.status(400).json({ message: "User already exist" });
@@ -32,8 +42,9 @@ export const register = async (req, res) => {
         photo,
         gender,
         role,
-      })}
-      if (role === "doctor") {
+      });
+    }
+    if (role === "doctor") {
       user = new Doctor({
         name,
         email,
@@ -42,18 +53,58 @@ export const register = async (req, res) => {
         gender,
         role,
       });
-
-      await user.save()
-
-      res.status(200).json({success:true, message:'User successfully created'})
-
     }
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "User successfully created" });
   } catch (error) {
-    res.status(200).json({success:false, message:'Internal server error'})
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
 export const login = async (req, res) => {
+  const { email } = req.body;
   try {
-  } catch (error) {}
+    let user = null;
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
+
+    if (patient) {
+      user = patient;
+    }
+    if (doctor) {
+      user = doctor;
+    }
+    // check if user exist or not
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    //compare password
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordMatch) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Invalid crendentials" });
+    }
+
+    //get Token
+    const token = generateToken(user);
+
+    const { password, role, appointments, ...rest } = user._doc;
+    res.status(200).json({
+      status: true,
+      message: "Successfuly login",
+      token,
+      data: { ...rest },
+      role,
+    });
+  } catch (error) {
+    res.status(404).json({ status: false, message: "Failed to login" });
+  }
 };
