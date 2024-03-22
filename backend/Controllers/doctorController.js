0bcbd582b47b46a4199db6ca4525cf18d56d45cc
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-03-22 00:00:36 +0000

    add DoctorController and Doctorfile

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -57,11 +57,25 @@ export const getSingleDoctor = async (req, res) => {
 
 export const getAllDoctor = async (req, res) => {
   try {
-    const doctors = await Doctor.find({}).select("-password");
+    const { query } = req.query;
+    let doctors;
+    if (query) {
+      doctors = await Doctor.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { specialization: { $regex: query, $options: "i" } },
+        ],
+      }).select("-password");
+    } else {
+      doctors = await Doctor.find({ isApproved: "approved" }).select(
+        "-password"
+      );
+    }
     // console.log(Doctors);
     res.status(200).json({
       success: true,
-      message: "Doctors Found",
+      message: "Users Found",
       data: doctors,
     });
   } catch (err) {
