a540e63d36bd646b31ba95fc8ceea00df1947091
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-03-24 16:05:31 +0000

    add DashbordofUser and Doctor

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -7,9 +7,14 @@ import Signup from "../pages/Signup";
 import Contact from "../pages/Contact";
 import Services from "../pages/Services";
 import { Routes, Route } from "react-router-dom";
+import MyAccount from "../Dashboard/user-account/MyAccount";
+import Dashboard from "../Dashboard/doctor-account/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+
 const Routers = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/doctors" element={<Doctors />} />
       <Route path="/doctors/:id" element={<DoctorDetails />} />
@@ -17,6 +22,22 @@ const Routers = () => {
       <Route path="/register" element={<Signup />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/services" element={<Services />} />
+      <Route
+        path="/users/profile/me"
+        element={
+          <ProtectedRoute allowedRoles={["patient"]}>
+            <MyAccount />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/doctors/profile/me"
+        element={
+          <ProtectedRoute allowedRoles={["doctor"]}>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
