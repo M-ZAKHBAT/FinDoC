a67e852dc95486ca3f1372dd530e6ad2c00369db
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-03-28 00:03:11 +0000

    add ProfileDoctor

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -5,13 +5,16 @@ import useGetProfile from "../../hooks/useFetchData";
 import { BASE_URL } from "../../config";
 import Tabs from "./Tabs";
 import { useState } from "react";
+import starIcon from "../../assets/images/Star.png";
+import DoctorAbout from "./../../pages/Doctors/DoctorAbout";
+import Profile from "./Profile";
 
 const Dashboard = () => {
   const { data, loading, error } = useGetProfile(
     `${BASE_URL}/doctors/profile/me`
   );
-
-  const [tab, setTab] = useState("overview");
+  // const [tab, setTab] = useState("overview");
+  const [tab, setTab] = useState("");
   return (
     <section>
       <div className="max-w-[1170px] px-5 mx-auto">
@@ -43,6 +46,48 @@ const Dashboard = () => {
                   </div>
                 </div>
               )}
+
+              <div className="mt-8">
+                {tab === "overview" && (
+                  <div>
+                    <div className="flex items-center gap-4 mb-10">
+                      <figure className="max-w-[200px] max-h-[200px]">
+                        <img src={data?.photo} alt="" className="w-full" />
+                      </figure>
+
+                      <div>
+                        <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
+                          {data.specialization} Surgeon
+                        </span>
+                        <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">
+                          SANABI MOHAMED
+                        </h3>
+                        <div className="flex items-center gap-[6px]">
+                          <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold ">
+                            <img src={starIcon} alt="" />
+                            4.5
+                          </span>
+                          <span className=" text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold ">
+                            (233)
+                          </span>
+                        </div>
+                        <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
+                          Doctor bio
+                        </p>
+                      </div>
+                    </div>
+                    <DoctorAbout
+                      name={data.name}
+                      about={data.about}
+                      qualifications={data.qualifications}
+                      experiences={data.experiences}
+                    />
+                  </div>
+                )}
+
+                {tab === "appoitments" && <div>appointments</div>}
+                {tab === "settings" && <Profile />}
+              </div>
             </div>
           </div>
         )}
---
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -0,0 +1,176 @@
+// import React from 'react'
+
+import { useState } from "react";
+
+const Profile = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+    gender: "",
+    specialization: "",
+    ticketPrice: "0",
+    qualifications: [
+      { startingDate: "", endingDate: "", degree: "", university: "" },
+    ],
+    experiences: [],
+    timeSlots: [],
+  });
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  return (
+    <div>
+      <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10 ">
+        Profile Information
+      </h2>
+      <form>
+        <div className="mb-5">
+          <p className="form__label">Name</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="full Name"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Email</p>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+            className="form__input"
+            readOnly
+            aria-readonly
+            disabled="true"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Phone</p>
+          <input
+            type="number"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+            placeholder="Phone number"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Bio</p>
+          <input
+            type="text"
+            name="bio"
+            value={formData.phone}
+            onChange={handleInputChange}
+            placeholder="Bio"
+            className="form__input"
+            maxLength={100}
+          />
+        </div>
+        <div className="mb-5">
+          <div className="grid grid-cols-3 gap-5 mb-[30px]">
+            <div>
+              <p className="form__label">Gender</p>
+              <select
+                name="gender"
+                value={formData.gender}
+                onChange={handleInputChange}
+                className="form__input py-3.5"
+              >
+                <option value="">Select</option>
+                <option value="">Male</option>
+                <option value="">Femal</option>
+              </select>
+            </div>
+
+            <div>
+              <p className="form__label">Specialization</p>
+              <select
+                name="specialization"
+                value={formData.specialization}
+                onChange={handleInputChange}
+                className="form__input py-3.5"
+              >
+                <option value="">Select</option>
+                <option value="surgeon">Surgeon</option>
+                <option value="neurologist">Neurologist</option>
+                <option value="dermatologist">Dermatologist</option>
+              </select>
+            </div>
+
+            <div>
+              <p className="form__label">Ticket Price+</p>
+              <input
+                type="number"
+                placeholder="100"
+                name="ticketPrice"
+                value={formData.ticketPrice}
+                className="form__input"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-5">
+          <p className="form__label">Qualifications</p>
+          {formData.qualifications?.map((item, index) => (
+            <div key={index}>
+              <div>
+                <div className="grid grid-cols-2 gap-5">
+                  <div>
+                    <p className="form__label"> Starting Date+</p>
+                    <input
+                      type="date"
+                      name="staringDate"
+                      value={item.startingDate}
+                      className="form__input"
+                    />
+                  </div>
+                  <div>
+                    <p className="form__label"> Ending Date+</p>
+                    <input
+                      type="date"
+                      name="staringDate"
+                      value={item.endingDate}
+                      className="form__input"
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 gap-5">
+                  <div>
+                    <p className="form__label"> Degree+</p>
+                    <input
+                      type="text"
+                      name="degree"
+                      value={item.degree}
+                      className="form__input"
+                    />
+                  </div>
+                  <div>
+                    <p className="form__label"> University+</p>
+                    <input
+                      type="text"
+                      name="university"
+                      value={item.university}
+                      className="form__input"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
