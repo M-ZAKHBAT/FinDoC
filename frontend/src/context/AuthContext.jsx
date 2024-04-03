fa4b3ade6d36d70b841397af0c04d72c4b4604bf
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-04-03 11:47:31 +0000

    add gestion de paiement avec stripe

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useReducer } from "react";
 
 const initialState = {
   user:
