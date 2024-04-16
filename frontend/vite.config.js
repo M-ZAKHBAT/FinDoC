a0763536a399f9e9080f7642c54751d14ad474ca
M-ZAKHBAT <zakhbat.mohammed@gmail.com>
2024-04-16 17:43:48 +0000

    add localversion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: "0.0.0.0",
+    port: 3000,
+  },
   // build: {
   //   rollupOptions: {
   //     input: {
